3f5bc78cb51ef057bb506b85a696cdb13416a56c
H. L. Nourse <32994022+thenoursehorse@users.noreply.github.com>
2024-02-02 16:18:43 +0900

    feat: generate and save truncated unitary to file

---
+++ kicked_bosons/run_unitary_truncated.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+njobs=10
+# Source any environment stuff you need
+source ${HOME}/venv/kicked-boson_qist/bin/activate
+#source $HOME/miniforge3/etc/profile.d/conda.sh
+#conda activate quantum-chaos
+
+# Export any variables that you want the bash function to be able to see
+export exec_folder=${HOME}/GitHub/quantum-chaos/kicked_bosons
+export root_folder=/scratch/NemotoU/henry/quantum-chaos/kicked_bosons
+
+# The parameters you want to parallelize over (can also get this from an external
+# parameter file)
+M_arr=(10 20 50 100 200 300 400 500)
+N_arr=(2)
+num_ensembles_arr=(100)
+num_repeats_arr=(1000)
+time_arr=($((10**12)) "heisenberg")
+theta_W_pairs=("7.4 7" "7.4 3.5" "7.4 2" "18 3" "20 0.5")
+
+# The shell script function GNU-parallel calls  
+unitary_truncated() {
+  # $1,$2,$3, etc are the parameters parallel passes, in the order you give them
+  M=$1
+  N=$2
+  num_ensembles=$3
+  num_repeats=$4
+  timee=$5
+
+  # Split pairs
+  set -- $6
+  thetaOmega=$1
+  WOmega=$2
+  
+  # stdout and stderr get redirected to this file
+  outfile=unitary_truncated_M${M}_N${N}_num_ensembles${g}_num_repeats${num_repeats}_time_${timee}_thetaOmega${thetaOmega}_WOmega${WOmega}.out
+
+  # This is basically the program I call, with the parameters I set
+  python3 -u ${exec_folder}/save_unitary_truncated.py \
+    -M ${M} \
+    -N ${N} \
+    -num_ensembles ${num_ensembles} \
+    -num_repeats ${num_repeats} \
+    -thetaOmega ${thetaOmega} \
+    -WOmega ${WOmega} \
+    -time ${timee} \
+    -root_folder ${root_folder} \
+    -save_data 1 \
+	  &> ${outfile}
+}
+# You must export the function for the shell script to know about it
+export -f unitary_truncated
+
+# Run in parallel using GNU parallel
+parallel -j${njobs} unitary_truncated ::: "${M_arr[@]}" ::: "${N_arr[@]}" ::: "${num_ensembles_arr[@]}" ::: "${num_repeats_arr[@]}" ::: "${time_arr[@]}" ::: "${theta_W_pairs[@]}"
---
+++ kicked_bosons/save_unitary_truncated.py
@@ -0,0 +1,142 @@
+import time
+import numpy as np
+
+import os
+import sys
+import argparse
+
+import h5py
+
+from quantum_chaos.quantum.kicked_bosons import KickedBosons
+
+def h5_wait(h5file, wait=3, max_wait=30):
+    
+    waited = 0
+
+    while True:
+        try:
+            h5f = h5py.File(h5file,'r')
+            break
+                
+        except FileNotFoundError:
+            print('\nError: HDF5 File not found\n')
+            return False
+        
+        except OSError:   
+            if waited < max_wait:
+                print(f'Warning: HDF5 File locked, sleeping {wait} seconds...')
+                time.sleep(wait) 
+                waited += wait  
+            else:
+                print(f'\nWaited too long = {waited} secs, exiting...\n')
+                return False
+
+    h5f.close()
+    return True
+
+def get_truncated_unitary(M, N, num_ensembles, theta, theta_disorder, Omega, phi_disorder, eta, eta_disorder, t=1e12):
+    start = time.time()
+    model = KickedBosons(M=M,
+                         num_ensembles=num_ensembles,
+                         theta=theta,
+                         Omega=Omega,
+                         eta=eta,
+                         theta_disorder=theta_disorder,
+                         phi_disorder=phi_disorder,
+                         eta_disorder=eta_disorder
+    )
+    end = time.time()
+    print("Unitary construction took", end-start)
+    
+    if t == 'heisenberg':
+        t = model.heisenberg_time()
+    else:
+        t = float(t)
+         
+    start = time.time()
+    model.set_unitary_evolve(num_ensembles=args.num_ensembles, time=np.array([t]))
+    end = time.time()
+    print("Time evolve unitaries took", end-start)
+    
+    # Take upper left corner of unitaries
+    ix = 0
+    iy = 0
+    return model._Ut[-1][:, ix:ix+args.N, iy:iy+args.N]
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-M', type=int, default=300)
+    parser.add_argument('-N', type=int, default=2)
+    parser.add_argument('-num_ensembles', type=int, default=10)
+    parser.add_argument('-num_repeats', type=int, default=1)
+    
+    parser.add_argument('-thetaOmega', type=float, default=7.4)
+    parser.add_argument('-thetaOmega_disorder', type=float, default=0.0)
+    
+    parser.add_argument('-Omega', type=float, default=np.pi/4.0)
+    parser.add_argument('-WOmega', type=float, default=2)
+    
+    parser.add_argument('-etaOmega', type=float, default=0)
+    parser.add_argument('-etaOmega_disorder', type=float, default=0)
+    
+    parser.add_argument('-time', type=str, default='1e12')
+    
+    parser.add_argument('-root_folder', type=str, default='./data/')
+    parser.add_argument('-save_data', type=int, default=0)
+    
+    args = parser.parse_args()
+    print(vars(args))
+    
+    ##########################################
+    ## Make parameters for Floquet operator ##
+    ##########################################
+    theta = args.thetaOmega / (16.0 * args.Omega)
+    theta_disorder = args.thetaOmega_disorder / (16.0 * args.Omega)
+    phi_disorder = args.WOmega / (16.0 * args.Omega)
+    eta = args.etaOmega / (16.0 * args.Omega)
+    eta_disorder = args.etaOmega_disorder / (16.0 * args.Omega)
+
+    for i in range(args.num_repeats):
+        print(f"Iteration {i+1}/{args.num_repeats}")
+
+        U_trunc = get_truncated_unitary(args.M, args.N, args.num_ensembles, theta, theta_disorder, args.Omega, phi_disorder, eta, eta_disorder, args.time)
+                            
+        if args.save_data:
+            if args.time == 'heisenberg':
+                folder = args.root_folder + f'/t_heis'
+            elif float(args.time) > 1e6:
+                folder = args.root_folder + f'/t_inf'
+            else:
+                folder = args.root_folder + f'/t_{float(args.time):.2f}'
+            os.makedirs(f'{folder}', exist_ok=True)
+            filename = folder + f'/thetaOmega{args.thetaOmega:.2f}WOmega{args.WOmega:.2f}_Utrunc.h5'
+
+            # Check that file is not in use
+            if os.path.isfile(filename):
+                h5stat = h5_wait(filename)
+                if h5stat is False:
+                    sys.exit('Error: HDF5 File not available')
+            
+            # Write real and imaginary parts separately
+            with h5py.File(filename, 'a') as f:
+                key = f'M{args.M}N{args.N}'
+                g = f.require_group(key)
+
+                key = 'real' 
+                if key not in g:
+                    d = g.create_dataset(key, data=U_trunc.real, maxshape=(None, args.N, args.N), dtype='float64')
+                else:
+                    d = g[key]
+                    size = d.shape[0] + U_trunc.shape[0]
+                    d.resize(size, axis=0)
+                    d[-U_trunc.shape[0]:] = U_trunc.real
+                
+                key = 'imag' 
+                if key not in g:
+                    d = g.create_dataset(key, data=U_trunc.imag, maxshape=(None, args.N, args.N), dtype='float64')
+                else:
+                    # Append to existing dataset
+                    d = g[key]
+                    size = d.shape[0] + U_trunc.shape[0]
+                    d.resize(size, axis=0)
+                    d[-U_trunc.shape[0]:] = U_trunc.imag
